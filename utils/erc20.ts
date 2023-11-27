a868c5678e30243f21a5d1b66f858c8684dcd76c
suha jin <89185836+djm07073@users.noreply.github.com>
2023-11-28 00:55:46 +0900

    utils fix

--- utils/erc20.ts
+++ utils/erc20.ts
@@ -1,9 +1,39 @@
 import { ethers, network } from "hardhat";
-import { ISwapRouter02 } from "./swap-router-contracts/contracts/interfaces";
-const ROUTER : Record<number,`0x${string}`> = {
-  "1": "0xE592427A0AEce92De3Edee1F18E0157C05861564",//uniswap v3 swaprouter
+const ROUTER: Record<number, `0x${string}`> = {
+  "1": "0xE592427A0AEce92De3Edee1F18E0157C05861564", //uniswap v3 swaprouter
 };
-
+const ROUTER_ABI: any[] = [
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: "address", name: "tokenIn", type: "address" },
+          { internalType: "address", name: "tokenOut", type: "address" },
+          { internalType: "uint24", name: "fee", type: "uint24" },
+          { internalType: "address", name: "recipient", type: "address" },
+          { internalType: "uint256", name: "amountIn", type: "uint256" },
+          {
+            internalType: "uint256",
+            name: "amountOutMinimum",
+            type: "uint256",
+          },
+          {
+            internalType: "uint160",
+            name: "sqrtPriceLimitX96",
+            type: "uint160",
+          },
+        ],
+        internalType: "struct IV3SwapRouter.ExactInputSingleParams",
+        name: "params",
+        type: "tuple",
+      },
+    ],
+    name: "exactInputSingle",
+    outputs: [{ internalType: "uint256", name: "amountOut", type: "uint256" }],
+    stateMutability: "payable",
+    type: "function",
+  },
+];
 export const approve = async (
   tokenAddr: `0x${string}`,
   amount: bigint,
@@ -20,13 +50,25 @@ export const approveMax = async (
   await approve(tokenAddr, ethers.MaxUint256, spender);
 };
 
-export const getTokens = async (tokenIn:`0x${string}`,tokenOut:`0x${string}`,amountIn:bigint) => {
+export const getBalance = async (tokenAddr: `0x${string}`) => {
+  const [signer] = await ethers.getSigners();
+  const token = await ethers.getContractAt("ERC20", tokenAddr);
+  return token.balanceOf(signer.address);
+};
+export const getTokens = async (
+  tokenIn: `0x${string}`,
+  tokenOut: `0x${string}`,
+  amountIn: bigint
+) => {
   if (network.config.chainId != 1) {
     console.log("mainnet only supported");
     return;
   }
   const routerAddr = ROUTER[1];
-  const router: ISwapRouter02 = await ethers.getContractAt("ISwapRouter02", routerAddr);
+  const router = await ethers.getContractAt(
+    ROUTER_ABI,
+    routerAddr
+  );
   const [signer] = await ethers.getSigners();
   await approveMax(tokenIn, routerAddr);
   return router.exactInputSingle({
@@ -38,5 +80,4 @@ export const getTokens = async (tokenIn:`0x${string}`,tokenOut:`0x${string}`,amo
     amountOutMinimum: 0,
     sqrtPriceLimitX96: 0,
   });
-}
-
+};
